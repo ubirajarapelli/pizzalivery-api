fffe91aa8137b24c3d1ec552baeb135e9736e636
Ubirajara Pelli <ubirajaracpelli@gmail.com>
2023-10-31 19:34:19 -0300

    add login scheme

--- router.js
+++ router.js
@@ -140,6 +140,26 @@ const router = [
       return response
     },
   },
+  {
+    method: "POST",
+    path: "/user/login",
+    handler: function (request, h) {
+      const payload = request.payload
+      if (
+        payload.login === "jorge@abreu.com.br" &&
+        payload.password === "abobrinha"
+      ) {
+        const token = {
+          token: Buffer.from(`${payload.login}.${payload.password}`).toString(
+            "base64"
+          ),
+        }
+        return token
+      }
+
+      return h.response({ message: "Usuário não encontrado" }).code(404)
+    },
+  },
 ]
 
 module.exports = router
